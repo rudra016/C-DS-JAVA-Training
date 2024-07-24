562079b8b7e035f491fc6f6929d76aefb3ba6b48
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2024-07-24 16:23:14 +0530

    Create StringOperations.java

---
+++ src/StringOperations.java
@@ -0,0 +1,11 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        if("rudra".equals(name)){
+            System.out.println("allowed");
+        }else{
+            System.out.println("not allowed");
+        }
+    }
+}
