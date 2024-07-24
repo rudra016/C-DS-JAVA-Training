640e2f030172f1f62c8c0673c4b319f3c375c5ef
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2024-07-24 16:09:03 +0530

    Create ForEach.java

---
+++ src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+    String[] names = {"Anshu", "Ivan", "Kunal", "Rini", "Mudit"};
+    for(String i : names){
+        System.out.println(i);
+    }
+//        for(int i = 0; i < names.length; i++){
+//            System.out.println(names[i]);
+//        }
+    }
+}
